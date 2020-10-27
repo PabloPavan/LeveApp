1e9ef60172688a94d72a3d4be42d4842c49f6ad3
pjpavan <ppavan@nelogica.com.br>
2020-10-27 18:48:45 -0300

    Adding dproj

---
+++ LeveApp.dpr
@@ -0,0 +1,16 @@
+﻿program LeveApp;
+
+{$R *.dres}
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  mainU in 'mainU.pas' {Main};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TMain, MainForm);
+  Application.Run;
+end.
